dff7a887f3dba014486f2d2b60353fb9805a7e42
Oliver Berzs <oliver.berzs@gmail.com>
2020-07-06 13:58:15 +0300

    :fire: Delete import-shaders.bat

--- import-shaders.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-import examples/gradient/assets/gradient.glsl -o examples/gradient/assets
-import examples/surface/assets/raymarch.glsl -o examples/surface/assets
-import -d tegne/assets/shaders/glsl -o tegne/assets/shaders
